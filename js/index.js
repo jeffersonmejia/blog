c8c58c0f579381198caae479bf5a34dc11c65a57
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2022-05-05 11:05:25 -0500

    validation to lastname and topic

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const d = document;
 const $formEl = {
 	$form: d.querySelector("form"),
 	$input: d.querySelectorAll(".input"),
+	$submit: d.querySelector("form .submit-on"),
 	$alert: {
 		$name: d.querySelector("form #alert-name"),
 		$lastname: d.querySelector("form #alert-lastname"),
@@ -11,13 +12,19 @@ const $formEl = {
 	},
 };
 
+let fill = {
+	name: false,
+	lastname: false,
+	topic: false,
+};
+
 const expression = {
 	names: {
 		first: /^[a-zA-Z]{3,16}/,
 		second: /\s[a-zA-Z]{3,16}/,
 		space: /\s/,
 	},
-	topic: /^.{32,64}$/,
+	topic: /^.{32,128}$/,
 };
 
 const alerts = {
@@ -27,10 +34,11 @@ const alerts = {
 		second: "Ingresa tu segundo nombre, por favor",
 	},
 	lastname: {
-		first: "Ingresa tu nombre, por favor",
-		second: "Ahora ingresa tu apellido",
+		first: "Ingresa tu apellido, por favor",
+		space: "Ahora ingresa tu segundo apellido",
+		second: "Ahora ingresa tu segundo apellido",
 	},
-	topic: "El asunto debe contener mínimo 32 palabras",
+	topic: "El asunto debe contener mínimo 32 carácteres",
 };
 
 $formEl.$form.addEventListener(
@@ -47,20 +55,46 @@ $formEl.$form.addEventListener(
 							$formEl.$alert.$name.innerHTML = alerts.name.second;
 						} else {
 							$formEl.$alert.$name.innerHTML = "";
+
+							// fill.name = true;
 						}
 					}
 				}
 				break;
 			}
 			case "lastname": {
-				console.log("it works");
+				if (!expression.names.first.test(e.target.value)) {
+					$formEl.$alert.$lastname.innerHTML = alerts.lastname.first;
+				} else {
+					if (expression.names.space.test(e.target.value)) {
+						$formEl.$alert.$lastname.innerHTML = alerts.lastname.space;
+						if (!expression.names.second.test(e.target.value)) {
+							$formEl.$alert.$lastname.innerHTML = alerts.lastname.second;
+						} else {
+							$formEl.$alert.$lastname.innerHTML = "";
+						}
+					}
+				}
 				break;
 			}
 			case "topic": {
-				console.log("it works");
+				if (!expression.topic.test(e.target.value)) {
+					$formEl.$alert.$topic.innerHTML = alerts.topic;
+				} else {
+					$formEl.$alert.$topic.innerHTML = "";
+				}
 				break;
 			}
+		}
+		if (fill.name === true) {
+			$formEl.$submit.classList.toggle("submit-on");
 		}
 	},
 	true
 );
+
+$formEl.$form.addEventListener;
+
+$formEl.$form.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
