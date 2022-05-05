09c42467c6c2e2886d518dd54deb08fe49ba2f20
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2022-05-04 23:22:56 -0500

    add validate to first input tottaly

--- js/index.js
+++ js/index.js
@@ -1,27 +1,66 @@
-// const $navEl = {
-// 	$nav: document.querySelector("ul li"),
-// 	$li: document.querySelectorAll("li .nav-unactive"),
-// };
+const d = document;
 
-// const $figEl = {
-// 	$fig: document.querySelector(".systems"),
-// 	$box: document.querySelectorAll(".systems .fig-unactive"),
-// };
+const $formEl = {
+	$form: d.querySelector("form"),
+	$input: d.querySelectorAll(".input"),
+	$alert: {
+		$name: d.querySelector("form #alert-name"),
+		$lastname: d.querySelector("form #alert-lastname"),
+		$topic: d.querySelector("form #alert-topic"),
+		$submit: d.querySelector("form #alert-submit"),
+	},
+};
 
-// //Pendiente aún... desactivar clases
-// $navEl.$nav.addEventListener("mouseover", (e) => {
-// 	if (!e.target.matches(".nav-active")) {
-// 		$navEl.$li.forEach((el) => {
-// 			el.classList.toggle("nav-active");
-// 		});
-// 	}
-// });
-// // Pendiente... hover no aplicado
-// // $figEl.$fig.addEventListener("mouseover", (e) => {
-// // 	if (!e.target.matches(".fig-active")) {
-// // 		console.log("it works");
-// // 		$figEl.$box.forEach((el) => {
-// // 			el.classList.toggle("fig-active");
-// // 		});
-// // 	}
-// // });
+const expression = {
+	names: {
+		first: /^[a-zA-Z]{3,16}/,
+		second: /\s[a-zA-Z]{3,16}/,
+		space: /\s/,
+	},
+	topic: /^.{32,64}$/,
+};
+
+const alerts = {
+	name: {
+		first: "Ingresa tu nombre, por favor",
+		space: "Ahora ingresa tu segundo nombre",
+		second: "Ingresa tu segundo nombre, por favor",
+	},
+	lastname: {
+		first: "Ingresa tu nombre, por favor",
+		second: "Ahora ingresa tu apellido",
+	},
+	topic: "El asunto debe contener mínimo 32 palabras",
+};
+
+$formEl.$form.addEventListener(
+	"keyup",
+	(e) => {
+		switch (e.target.id) {
+			case "name": {
+				if (!expression.names.first.test(e.target.value)) {
+					$formEl.$alert.$name.innerHTML = alerts.name.first;
+				} else {
+					if (expression.names.space.test(e.target.value)) {
+						$formEl.$alert.$name.innerHTML = alerts.name.space;
+						if (!expression.names.second.test(e.target.value)) {
+							$formEl.$alert.$name.innerHTML = alerts.name.second;
+						} else {
+							$formEl.$alert.$name.innerHTML = "";
+						}
+					}
+				}
+				break;
+			}
+			case "lastname": {
+				console.log("it works");
+				break;
+			}
+			case "topic": {
+				console.log("it works");
+				break;
+			}
+		}
+	},
+	true
+);
