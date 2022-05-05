9717e0c2984532a2d8f44265302dcb957a79265b
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2022-05-04 20:02:28 -0500

    add hover only desktop

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const $navEl = {
+	$nav: document.querySelector("ul li"),
+	$li: document.querySelectorAll("li .nav-unactive"),
+};
+
+const $figEl = {
+	$fig: document.querySelector(".systems"),
+	$box: document.querySelectorAll(".systems .fig-unactive"),
+};
+
+//Pendiente aún... desactivar clases
+$navEl.$nav.addEventListener("mouseover", (e) => {
+	if (!e.target.matches(".nav-active")) {
+		$navEl.$li.forEach((el) => {
+			el.classList.toggle("nav-active");
+		});
+	}
+});
+// Pendiente... hover no aplicado
+// $figEl.$fig.addEventListener("mouseover", (e) => {
+// 	if (!e.target.matches(".fig-active")) {
+// 		console.log("it works");
+// 		$figEl.$box.forEach((el) => {
+// 			el.classList.toggle("fig-active");
+// 		});
+// 	}
+// });
